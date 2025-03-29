8c35e0c91d0208c1e2f3ab0705cdc0bdc9e50c99
Clement-Salvador <salvadoc@3il.fr>
2025-03-29 16:28:18 +0100

    FIX : Récupération de 20 PV et non de 20%

--- Simulateur/CanardEau.java
+++ Simulateur/CanardEau.java
@@ -1,14 +1,14 @@
 public class CanardEau extends Canard {
 
     public CanardEau(String nom, int pointsDeVie, int pointsAttaque, TypeCanard.typeCanard type) {
-        super(nom, pointsDeVie, pointsAttaque, type.CanardEau);
+        super(nom, pointsDeVie, pointsAttaque, type);
     }
 
     public void activerCapaciteSpeciale() {
         if (!isCapaciteSpecialeUtilisee()) {
             System.out.println(getNom() + " active sa capacité spéciale : Régénération Aquatique !");
-            // Calcul de la récupération : 20% des points de vie actuels.
-            int recuperation = (int) (getPointsDeVie() * 0.2);
+            // Récupère 20PV.
+            int recuperation = 20;
             setPointsDeVie(getPointsDeVie() + recuperation);
             markCapaciteSpecialeAsUsed(); // Marquer que la capacité spéciale a été utilisée.
         } else {
