c8666867aa0e57fa9cc368c8d94c577bd3f9dd22
Clement-Salvador <salvadoc@3il.fr>
2025-03-29 15:47:28 +0100

    FIX : Oubli d'un attaque faible contre défenseur.

--- Simulateur/TypeCanard.java
+++ Simulateur/TypeCanard.java
@@ -13,39 +13,38 @@ public class TypeCanard {
             // GLACE > VENT
             // VENT > EAU
 
-        // x 0.5 
+        // x 0.5 :
+            // EAU > GLACE, VENT
             // FEU > EAU
             // GLACE > FEU
             // VENT > GLACE
-            // EAU > VENT
 
         // x 1 sinon.
 
-        /**
-         * 
-         * 
-         * @param attaquant : Le canard attaquant.
-         * @param defenseur : Le canard défenseur.
-         * @return          : La valeur du multiplicateur.
-         */
-        public static double getMultiplicateur(typeCanard attaquant, typeCanard defenseur) {
-            // Cas : attaquant fort contre défenseur
-            if ((attaquant == typeCanard.CanardEau && defenseur == typeCanard.CanardFeu) ||
-                    (attaquant == typeCanard.CanardFeu && defenseur == typeCanard.CanardGlace) ||
-                    (attaquant == typeCanard.CanardGlace && defenseur == typeCanard.CanardVent) ||
-                    (attaquant == typeCanard.CanardVent && defenseur == typeCanard.CanardEau)) {
-                return 1.5;
-            }
-            // Cas : attaquant faible contre défenseur (inverse)
-            else if ((attaquant == typeCanard.CanardFeu && defenseur == typeCanard.CanardEau) ||
-                    (attaquant == typeCanard.CanardGlace && defenseur == typeCanard.CanardFeu) ||
-                    (attaquant == typeCanard.CanardVent && defenseur == typeCanard.CanardGlace) ||
-                    (attaquant == typeCanard.CanardEau && defenseur == typeCanard.CanardVent)) {
-                return 0.5;
-            }
-            // Sinon : neutre
-            else {
-                return 1.0;
-            }
+    /**
+     * 
+     * @param attaquant : Le canard attaquant.
+     * @param defenseur : Le canard défenseur.
+     * @return          : La valeur du multiplicateur.
+     */
+    public static double getMultiplicateur(typeCanard attaquant, typeCanard defenseur) {
+        // Cas : attaquant fort contre défenseur
+        if ((attaquant == typeCanard.CanardEau && defenseur == typeCanard.CanardFeu) ||
+            (attaquant == typeCanard.CanardFeu && defenseur == typeCanard.CanardGlace) ||
+            (attaquant == typeCanard.CanardGlace && defenseur == typeCanard.CanardVent) ||
+            (attaquant == typeCanard.CanardVent && defenseur == typeCanard.CanardEau)) {
+            return 1.5;
+        }
+        // Cas : attaquant faible contre défenseur
+        else if ((attaquant == typeCanard.CanardEau && (defenseur == typeCanard.CanardGlace || defenseur == typeCanard.CanardVent)) ||
+                 (attaquant == typeCanard.CanardFeu && defenseur == typeCanard.CanardEau) ||
+                 (attaquant == typeCanard.CanardGlace && defenseur == typeCanard.CanardFeu) ||
+                 (attaquant == typeCanard.CanardVent && defenseur == typeCanard.CanardGlace)) {
+            return 0.5;
+        }
+        // Sinon : neutre
+        else {
+            return 1.0;
         }
     }
+}
