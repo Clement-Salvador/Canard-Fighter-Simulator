31981a6867a786dcfe339da1a2ff8fe99afa6066
Clement-Salvador <salvadoc@3il.fr>
2025-03-29 17:01:30 +0100

    EDIT : Modification mise en forme doc.

--- Simulateur/CanardEau.java
+++ Simulateur/CanardEau.java
@@ -1,3 +1,17 @@
+/**
+ * Classe représentant un canard de type Eau
+ * 
+ * Le canard de type Eau possède une capacité spéciale appelée
+ * Régénération Aquatique, qui lui permet de régénérer 20
+ * points de vie
+ * lorsqu'elle est activée. Cette capacité ne peut être utilisée qu'une seule
+ * fois par combat.
+ * 
+ * Hérite de la classe abstraite {@link Canard}, qui définit les attributs
+ * communs à tous les types de canards.
+ *  
+ */
+
 public class CanardEau extends Canard {
 
     public CanardEau(String nom, int pointsDeVie, int pointsAttaque, TypeCanard.typeCanard type) {
