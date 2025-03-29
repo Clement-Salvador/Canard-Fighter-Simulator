bde69161934af05356dfb2ba2ccc4b3e0495a967
Clement-Salvador <salvadoc@3il.fr>
2025-03-29 16:31:00 +0100

    EDIT : Intégration capacité spécialeCanardFeu

--- Simulateur/CanardFeu.java
+++ Simulateur/CanardFeu.java
@@ -1,14 +1,40 @@
+/**
+ * Représente un canard de type Feu.
+ * Sa capacité spéciale inflige des dégâts supplémentaires et applique l'effet brûlure,
+ * qui inflige 10 dégâts supplémentaires au tour suivant.
+ */
 public class CanardFeu extends Canard {
 
-    public CanardFeu(String nom, int pointsDeVie, int pointsAttaque, TypeCanard.typeCanard type, boolean activerCapaciteSpeciale) {
-        super(nom, pointsDeVie, pointsAttaque, type);
-        //TODO Auto-generated constructor stub
+    private boolean brulureActive = false;
+
+    public CanardFeu(String nom, int pointsDeVie, int pointsAttaque) {
+        super(nom, pointsDeVie, pointsAttaque, TypeCanard.typeCanard.CanardFeu);
     }
 
+    /**
+     * Capacité spéciale : Flamme brûlante.
+     * Inflige des dégâts immédiats et applique un effet de brûlure pour un tour.
+     */
     @Override
     public void activerCapaciteSpeciale() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'activerCapaciteSpeciale'");
+        if (!isCapaciteSpecialeUtilisee()) {
+            System.out.println(getNom() + " lance Flamme brûlante !");
+            brulureActive = true;
+            markCapaciteSpecialeAsUsed();
+        } else {
+            System.out.println(getNom() + " a déjà utilisé sa capacité spéciale !");
+        }
     }
 
+    /**
+     * Applique l'effet de brûlure si actif, inflige 10 dégâts au canard ciblé.
+     * @param cible Le canard ciblé par l'effet brûlure.
+     */
+    public void appliquerEffetBrulure(Canard cible) {
+        if (brulureActive) {
+            System.out.println(cible.getNom() + " subit une brûlure ! -10 PV");
+            cible.subirDegats(10);
+            brulureActive = false; // effet pour un seul tour
+        }
+    }
 }
