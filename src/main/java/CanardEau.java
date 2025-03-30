78895676c9d968a1d9dd5696d61abd8108c327ef
Clément SALVADOR <salvadoc@3il.fr>
2025-03-30 11:59:28 +0200

    EDIT : Ajout param CanardCible

--- src/main/java/CanardEau.java
+++ src/main/java/CanardEau.java
@@ -9,22 +9,20 @@
  * 
  * Hérite de la classe abstraite {@link Canard}, qui définit les attributs
  * communs à tous les types de canards.
- *  
+ * 
  */
 
 public class CanardEau extends Canard {
 
     public CanardEau(String nom, int pointsDeVie, int pointsAttaque) {
-        super(nom, pointsDeVie, pointsAttaque,TypeCanard.typeCanard.CanardEau);
+        super(nom, pointsDeVie, pointsAttaque, TypeCanard.typeCanard.CanardEau);
     }
 
-    public void activerCapaciteSpeciale() {
+    public void activerCapaciteSpeciale(Canard cible) {
         if (!isCapaciteSpecialeUtilisee()) {
             System.out.println(getNom() + " active sa capacité spéciale : Régénération Aquatique !");
-            // Récupère 20PV.
-            int recuperation = 20;
-            setPointsDeVie(getPointsDeVie() + recuperation);
-            markCapaciteSpecialeAsUsed(); // Marquer que la capacité spéciale a été utilisée.
+            setPointsDeVie(getPointsDeVie() + 20);
+            markCapaciteSpecialeAsUsed();
         } else {
             System.out.println("La capacité spéciale de " + getNom() + " a déjà été utilisée !");
         }
